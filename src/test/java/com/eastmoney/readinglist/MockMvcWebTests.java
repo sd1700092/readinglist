8db30152f8299c454a50b088b5efb03f97a1e633
sd1700092 <foreverskyone@hotmail.com>
2018-11-02 19:54:53 +0800

    ch4.2.1

---
+++ src/test/java/com/eastmoney/readinglist/MockMvcWebTests.java
@@ -0,0 +1,44 @@
+package com.eastmoney.readinglist;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * @author Administrator
+ * created: 2018-11-02 19:34
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ReadingListApplication.class)
+@WebAppConfiguration
+public class MockMvcWebTests {
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+    }
+
+    @Test
+    public void homePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/readingList"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("readingList"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("books"))
+                .andExpect(MockMvcResultMatchers.model().attribute("books", Matchers.is(Matchers.empty())));
+    }
+}
